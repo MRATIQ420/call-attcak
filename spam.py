758c0e236a52e43d259335be97a89f479c4ed454
MRATIQ420 <67003289+MRATIQ420@users.noreply.github.com>
2020-07-11 06:51:44 +0500

    Update spam.py

--- spam.py
+++ spam.py
@@ -80,9 +80,9 @@ def call():
 banner="""
         \033[1;97m SPAM WA & CALL
         \33[90m --------------
-\033[1;97m[\033[1;96m!\033[1;97m]AUTHOR :\033[1;96mFAHMIAPZ
-\033[1;97m[\033[1;96m!\033[1;97m]YOUTUBE:\033[1;96mAPMZCHANNEL
-\033[1;97m[\033[1;96m!\033[1;97m]GITHUB :\033[4;92mgithub.com/BangDanz\033[00m
+\033[1;97m[\033[1;96m!\033[1;97m]AUTHOR :\033[1;96mBLACK RANJHA
+\033[1;97m[\033[1;96m!\033[1;97m]YOUTUBE:\033[1;96mNITE POINT
+\033[1;97m[\033[1;96m!\033[1;97m]YOUTUBE LINK :\033[4;92mhttps://www.youtube.com/channel/UCPjsRDBYr8KIR-3iMs00zmg\033[00m
 _____________________________________"""
 if __name__ == '__main__':
     os.system("clear")
